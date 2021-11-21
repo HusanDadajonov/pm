a6c04d6cca3fe915476a30d774ef45db1978ca8e
am1rcek <xusandadajonov4385@gmail.com>
2021-11-21 12:45:01 +0500

    awda

--- js/main.js
+++ js/main.js
@@ -3,9 +3,20 @@ let elHeaderBlock = document.querySelector(".header__block");
 let elBurgerBtn = document.querySelector(".header__burger-btn");
 let elNav = document.querySelector(".nav");
 let elCloseNav = document.querySelector(".nav__close");
+let elAcardionBtn = document.querySelector(".card__btn");
+let elAccardion = document.querySelector(".services__accardion");
+let elAcardionClose = document.querySelector(".card__btn-close")
 
 elBurgerBtn.addEventListener("click", function(){
     elNav.classList.add("show-nav");
+});
+
+elAcardionBtn.addEventListener("click",function(){
+    elAccardion.classList.add("show-accardion");
+});
+
+elAcardionClose.addEventListener("click",function(){
+    elAccardion.classList.remove("show-accardion");
 });
 
 elShopBtn.addEventListener("click", function(){
