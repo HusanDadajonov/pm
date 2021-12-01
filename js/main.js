c53cf94d0c8c672a4adb744a0d94d981cd189b1f
am1rcek <xusandadajonov4385@gmail.com>
2021-12-01 07:21:55 +0500

    awdd

--- js/main.js
+++ js/main.js
@@ -1,29 +1,69 @@
-let elShopBtn = document.querySelector(".header__shop-btn");
-let elHeaderBlock = document.querySelector(".header__block");
 let elBurgerBtn = document.querySelector(".header__burger-btn");
-let elNav = document.querySelector(".nav");
-let elCloseNav = document.querySelector(".nav__close");
-let elAcardionBtn = document.querySelector(".card__btn");
-let elAccardion = document.querySelector(".services__accardion");
-let elAcardionClose = document.querySelector(".card__btn-close")
+let elNavBlock = document.querySelector(".nav__block");
+let elNavMenuBtn = document.querySelector(".nav__menu-btn");
+let elBodyBg = document.querySelector(".body__bg");
+let elAboutBtn = document.querySelector(".about__btn");
+let elRemovedDesc = document.querySelectorAll(".about__desc--remove");
+let elShowDescOne = document.querySelector(".about__desc--show1");
+let elShowDescTwo = document.querySelector(".about__desc--show2");
+let elCardBtn = document.querySelector(".card__btn");
+let elAccardion = document.querySelector(".acardion");
+let elServiceList = document.querySelector(".services__list");
+let sum = 1;
+let sum2 = 1;
 
-elBurgerBtn.addEventListener("click", function(){
-    elNav.classList.add("show-nav");
-});
+elBurgerBtn.addEventListener("click", moreFunc);
+elNavMenuBtn.addEventListener("click", hiddenFunc);
+elAboutBtn.addEventListener("click", showMore);
+elCardBtn.addEventListener("click", showCards);
 
-elAcardionBtn.addEventListener("click",function(){
-    elAccardion.classList.add("show-accardion");
-});
+function moreFunc(e){
+    elNavBlock.style.transform = "translateX(0px)";
+    elBodyBg.style.opacity = "0.4";
+    elBodyBg.style.visibility = "visible";
+    elBodyBg.style.zIndex = "400";
+}
 
-elAcardionClose.addEventListener("click",function(){
-    elAccardion.classList.remove("show-accardion");
-});
+function hiddenFunc(e){
+    elNavBlock.style.transform = "translateX(100%)";
+    elBodyBg.style.opacity = "0";
+    elBodyBg.style.visibility = "hidden";
+    elBodyBg.style.zIndex = "-1";
+}
 
-elShopBtn.addEventListener("click", function(){
-    elHeaderBlock.classList.toggle("show-header-block");
-});
+function showMore(e){
+    if(sum == 1){
+        for(let i = 0; i < elRemovedDesc.length; i++){
+            elRemovedDesc[i].style.display = "none";
+        }
+        elShowDescOne.innerHTML = "Наша Компания долгое время работает на рынке Тошкент город.Опыт наших мастеров в сфере услуг по ремонту и реставрации мебели более 10 лет.";
+        elShowDescTwo.innerHTML = "Мы оказываем следующие услуги: ремонт мебели, реставрация мебели,обивка мебели,перетяжка мебели. Услуги по ремонту мебели,обивке мебели,перетяжке мебели, осуществляем как на дому,так и в нашей мастерской,производим реставрацию старинной,антикварной и современной мебели,профессионально восстановим Вашу мягкую,корпусную или кожаную мебель и на все выполненные работы предоставим гарантию";
+        elAboutBtn.innerHTML = "Закрыть";
+        sum = 0;
+    }
 
-elCloseNav.addEventListener("click",function (){
-    elNav.classList.remove("show-nav");
-})
+    else if(sum == 0){
+        for(let i = 0; i < elRemovedDesc.length; i++){
+            elRemovedDesc[i].style.display = "block";
+        }
+        elShowDescOne.innerHTML = "";
+        elShowDescTwo.innerHTML = "";
+        elAboutBtn.innerHTML = "Подробный";
+        sum = 1;
+    }
+}
 
+function showCards(e){
+    elServiceList.classList.toggle("padding-none");
+    if(sum2 == 1){
+        elCardBtn.innerHTML = "Закрыть";
+        elAccardion.style.display = "grid";
+        sum2 = 0
+    }
+
+    else if(sum2 == 0){
+        elCardBtn.innerHTML = "Подробный";
+        elAccardion.style.display = "none";
+        sum2 = 1;
+    }
+}
